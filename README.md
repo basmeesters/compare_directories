8fe5312b879fe820b2b7ec6e1c2eabd969651ed0
basmeesters <meesters.bas@gmail.com>
2017-08-25 15:34:24 +0200

    Remove print statements

--- README.md
+++ README.md
@@ -3,8 +3,8 @@ Compare Directories
 
 Simple utility tool that can check two directories and copies all the files
 present in the first, but not in the second (including sub directories). Files
-are checked on their name and size, so potentially there are some false
-negatives.
+are checked on their name and size, so potentially - but unlikely - there are
+some false negatives.
 
 You can run the tool by using the following line in the command line:
 
--- compare_directories.rb
+++ compare_directories.rb
@@ -14,15 +14,18 @@ end
 def start_comparing(dir1, dir2, target_dir)
   print "start getting files from #{dir1}\n"
   files1 = get_all(dir1)
-  files1.each do |f|
-    print f.to_s
-  end
+
+  # For debugging
+  # files1.each do |f|
+  #   print f.to_s
+  # end
 
   print "\nstart getting files from #{dir1}\n"
   files2 = get_all(dir2)
-  files2.each do |f|
-    print f.to_s
-  end
+  # For debugging
+  # files2.each do |f|
+  #   print f.to_s
+  # end
 
   difference = files1 - files2
   print "\nstart copying the difference\n"
@@ -51,7 +54,7 @@ def copy_files(bfiles, target_dir)
   Dir.mkdir(target_dir) unless File.exists?(target_dir)
   bfiles.each do |f|
     copy_file(f.file_location, "#{target_dir}/#{f.file_name}")
-    print "copied: #{f.file_location}\n"
+    # print "copied: #{f.file_location}\n"
   end
 end
 
